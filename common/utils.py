d651548f1981272ccc529d2e50a379dd4b7325dd
Fedortsov-AV <aleksandrfedorcov431@gmail.com>
2022-02-12 17:07:21 +1000

    Добавлены проверка типа данных для функции get_message

--- common/utils.py
+++ common/utils.py
@@ -8,11 +8,14 @@ def send_message(msg: dict, sock: socket.socket):
     if isinstance(msg, dict) and isinstance(sock, socket.socket):
         json_msg = json.dumps(msg).encode(ENCODE)
         sock.send(json_msg)
-    else:
-        return print('Неверный тип данных передан в аргументы функции send_message(msg: dict, sock: socket.socket)')
+        return
+    return print('Неверный тип данных передан в аргументы функции send_message(msg: dict, sock: socket.socket)')
 
-def get_message(sock):
-    byte_json_msg = sock.recv(MAX_LEN_MSG)
-    json_msg = byte_json_msg.decode(ENCODE)
-    data = json.loads(json_msg)
-    return data
+
+def get_message(sock: socket.socket):
+    if isinstance(sock, socket.socket):
+        byte_json_msg = sock.recv(MAX_LEN_MSG)
+        json_msg = byte_json_msg.decode(ENCODE)
+        data = json.loads(json_msg)
+        return data
+    return print('Неверный тип данных передан в аргументы функции sget_message(sock: socket.socket)')
