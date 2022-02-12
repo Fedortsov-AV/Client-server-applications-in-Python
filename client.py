d816aed1a41fa5a5b7fe14208a6a3246c5aa9fcb
Fedortsov-AV <aleksandrfedorcov431@gmail.com>
2022-02-12 16:58:22 +1000

    Добавлены проверка типа данных для функции send_message

--- client.py
+++ client.py
@@ -1,10 +1,8 @@
 import sys
 from socket import socket, AF_INET, SOCK_STREAM
 from common.utils import send_message, get_message
-from common.variables import DEFAULT_PORT, DEFAULT_ADR, VALID_ADR, VALID_PORT, MAX_LEN_MSG, ACTION, USER
+from common.variables import DEFAULT_PORT, DEFAULT_ADR, VALID_ADR, VALID_PORT, MAX_LEN_MSG, ALERT, ACTION, USER, RESPONCE
 
-print(sys.argv)
-print(VALID_ADR.findall(sys.argv[1]))
 
 def presence_msg():
     msg = {
@@ -15,7 +13,6 @@ def presence_msg():
 
 if VALID_ADR.findall(sys.argv[1]):
     ADDRES = VALID_ADR.findall(sys.argv[1])[0]
-    print(ADDRES)
 else:
     ADDRES = DEFAULT_ADR
     print('Установлен ip-адрес \'%s\', в строке параметров отсутствует задание ip-адреса соответствующее шаблону' % (
@@ -23,19 +20,17 @@ else:
 
 if VALID_PORT.findall(sys.argv[2]):
     PORT = int(VALID_PORT.findall(sys.argv[2])[0])
-    print(PORT)
 else:
     PORT = int(DEFAULT_PORT)
     print('Установлен ip-адрес \'%s\', в строке параметров отсутствует задание ip-адреса соответствующее шаблону' % (
         DEFAULT_ADR))
 
-print('%s:%d' % (ADDRES, PORT))
+#print('%s:%d' % (ADDRES, PORT))
 SRVSOCK = socket(AF_INET, SOCK_STREAM)
 SRVSOCK.connect((ADDRES, PORT))
 PRESENCE_MSG = presence_msg()
-PRESENCE_MSG = 'ACTION'
 send_message(PRESENCE_MSG, SRVSOCK)
 DATA = get_message(SRVSOCK)
-print(DATA)
+print(DATA[RESPONCE], DATA[ALERT])
 
 
--- common/utils.py
+++ common/utils.py
@@ -1,12 +1,14 @@
 import json
+import socket
 
 from common.variables import MAX_LEN_MSG, ENCODE
 
 
-def send_message(msg, sock):
-    json_msg = json.dumps(msg).encode(ENCODE)
-    sock.send(json_msg)
-
+def send_message(msg: dict, sock: socket.socket):
+    if isinstance(msg, dict) and isinstance(sock, socket.socket):
+        json_msg = json.dumps(msg).encode(ENCODE)
+        sock.send(json_msg)
+    return print('Неверный тип данных передан в аргументы функции send_message(msg: dict, sock: socket.socket)')
 
 def get_message(sock):
     byte_json_msg = sock.recv(MAX_LEN_MSG)
