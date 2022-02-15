9aaa5974a6073ef8810c498099ed79bd52af6f24
Fedortsov-AV <aleksandrfedorcov431@gmail.com>
2022-02-15 22:53:54 +1000

    Парсинг командной строки в client.py вынесен в отдельные функции

--- client.py
+++ client.py
@@ -35,28 +35,44 @@ def parcing_msg(input_date: dict):
             return 'Неправильный ответ/JSON-объект'
 
 
-def main():
-    if VALID_ADR.findall(sys.argv[1]):
-        ADDRES = VALID_ADR.findall(sys.argv[1])[0]
+def parce_addres_in_cmd(arg: list):
+    if VALID_ADR.findall(arg[1]):
+        if VALID_ADR.findall(arg[1])[0]:
+            ADDRES = VALID_ADR.findall(arg[1])[0]
+            return ADDRES
+        else:
+            ADDRES = DEFAULT_ADR
+            return ADDRES
     else:
         ADDRES = DEFAULT_ADR
         print(
             'Установлен ip-адрес \'%s\', в строке параметров отсутствует задание ip-адреса соответствующее шаблону' % (
                 DEFAULT_ADR))
+        return ADDRES
 
-    if VALID_PORT.findall(sys.argv[2]):
-        PORT = int(VALID_PORT.findall(sys.argv[2])[0])
+
+def parce_port_in_cmd(arg: list):
+    if VALID_PORT.findall(arg[2]):
+        if VALID_PORT.findall(arg[2])[0]:
+            PORT = int(VALID_PORT.findall(arg[2])[0])
+            return PORT
+
     else:
         PORT = int(DEFAULT_PORT)
         print(
             'Установлен ip-адрес \'%s\', в строке параметров отсутствует задание ip-адреса соответствующее шаблону' % (
                 DEFAULT_ADR))
+        return PORT
+
+
+def main():
+    ADDRES = parce_addres_in_cmd(sys.argv)
+    PORT = parce_port_in_cmd(sys.argv)
 
     # print('%s:%d' % (ADDRES, PORT))
     SRVSOCK = socket(AF_INET, SOCK_STREAM)
     SRVSOCK.connect((ADDRES, PORT))
     PRESENCE_MSG = presence_msg()
-    print(PRESENCE_MSG)
     send_message(PRESENCE_MSG, SRVSOCK)
     data = get_message(SRVSOCK)
     print(parcing_msg(data))
