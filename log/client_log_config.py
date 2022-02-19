3822646a3e630d96baa30237ca50502e1c4a6c7c
Fedortsov-AV <aleksandrfedorcov431@gmail.com>
2022-02-19 21:47:14 +1000

    Добавлено логирование для функций server.py

--- log/client_log_config.py
+++ log/client_log_config.py
@@ -1,15 +1,33 @@
 import logging
 import logging.handlers as hand
+import os
+import sys
 
-client_log = logging.getLogger('server')
-client_log.setLevel(logging.DEBUG)
+dir = os.path.dirname(os.path.abspath(__file__))
+file = os.path.join(dir, 'client.log')
+
+client_log = logging.getLogger('client')
 
 fmt = logging.Formatter("%(asctime)s %(levelname)s %(module)s - %(message)s", "%d.%m.%Y %H:%M:%S")
+client_log.setLevel(logging.DEBUG)
 
-rotation = hand.TimedRotatingFileHandler("server.log.", when="M", interval=1, encoding='UTF-8', delay=False)
+rotation = hand.TimedRotatingFileHandler(file, when="D", interval=1, encoding='UTF-8', delay=True)
+rotation.setLevel(logging.DEBUG)
+stream = logging.StreamHandler(sys.stdout)
+stream.setLevel(logging.DEBUG)
+
 rotation.setFormatter(fmt)
-client_log.addHandler(rotation)
-
-stream = logging.StreamHandler()
 stream.setFormatter(fmt)
+
+
+client_log.addHandler(rotation)
 client_log.addHandler(stream)
+
+
+if __name__ == '__main__':
+    client_log.debug('This is a debug message')
+    client_log.info('This is an info message')
+    client_log.warning('This is a warning message')
+    client_log.error('This is an error message')
+    client_log.critical('This is a critical message')
+    client_log.info('Тестовый запуск логирования')
--- log/server_log_config.py
+++ log/server_log_config.py
@@ -1,15 +1,33 @@
 import logging
 import logging.handlers as hand
+import os
+import sys
+dir = os.path.dirname(os.path.abspath(__file__))
+file = os.path.join(dir, 'server.log')
 
 server_log = logging.getLogger('server')
-server_log.setLevel(logging.DEBUG)
 
 fmt = logging.Formatter("%(asctime)s %(levelname)s %(module)s - %(message)s", "%d.%m.%Y %H:%M:%S")
+server_log.setLevel(logging.DEBUG)
 
-rotation = hand.TimedRotatingFileHandler("server.log.", when="M", interval=1, encoding='UTF-8', delay=False)
+rotation = hand.TimedRotatingFileHandler(file, when="D", interval=1, encoding='UTF-8', delay=True)
+rotation.setLevel(logging.DEBUG)
+stream = logging.StreamHandler(sys.stdout)
+stream.setLevel(logging.DEBUG)
+
 rotation.setFormatter(fmt)
-server_log.addHandler(rotation)
-
-stream = logging.StreamHandler()
 stream.setFormatter(fmt)
+
+
+server_log.addHandler(rotation)
 server_log.addHandler(stream)
+
+
+if __name__ == '__main__':
+    server_log.debug('This is a debug message')
+    server_log.info('This is an info message')
+    server_log.warning('This is a warning message')
+    server_log.error('This is an error message')
+    server_log.critical('This is a critical message')
+    server_log.info('Тестовый запуск логирования')
+
