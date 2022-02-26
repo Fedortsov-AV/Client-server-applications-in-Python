25a5f3ca47510df0889a618b46ae1eb334e4ebec
Fedortsov-AV <aleksandrfedorcov431@gmail.com>
2022-02-27 02:51:06 +1000

    Выполнено практическое задание к 7-му уроку

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import logging
 import sys
 import time
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 import log.client_log_config
 from common.utils import send_message, get_message
 from common.variables import PASSWORD, TIME, ACCOUNT_NAME, DEFAULT_PORT, DEFAULT_ADR, VALID_ADR, VALID_PORT, ALERT, \
-    ACTION, USER, RESPONSE
+    ACTION, USER, RESPONSE, MESSAGE_TEXT, FROM
 from decorator import logs
 
 logger = logging.getLogger('client')
@@ -24,18 +24,33 @@ def presence_msg():
     logger.debug(f'сформировано сообщение {msg}')
     return msg
 
+@logs
+def generation_msg(message: str, name: str) -> dict:
+    if message == 'exit':
+        logger.info('Закрываю скрипт')
+        sys.exit(0)
+    msg = {
+        ACTION: 'MESSAGE',
+        TIME: time.time(),
+        ACCOUNT_NAME: name,
+        MESSAGE_TEXT: message
+    }
+    logger.debug(f'сформировано сообщение {msg}')
+    return msg
 
 @logs
 def parsing_msg(input_date: dict):
+    print('parsing', input_date)
     # logger.debug(f'Получен аргумент {input_date}')
     try:
         if isinstance(input_date, dict):
-            if input_date[RESPONSE] and input_date[ALERT] and input_date[TIME]:
+            if input_date[RESPONSE] == 200 or input_date[RESPONSE] == 400 and input_date[ALERT] and input_date[TIME]:
                 if isinstance(input_date[TIME], float):
                     timeserv = time.strftime('%d.%m.%Y %H:%M', time.localtime(input_date[TIME]))
                     return f'{timeserv} - {input_date[RESPONSE]} : {input_date[ALERT]}'
                 raise ValueError
-            raise KeyError
+            elif input_date[RESPONSE] == 'message' and input_date[FROM] and input_date[MESSAGE_TEXT]:
+                return f"{input_date[FROM]} написал: {input_date[MESSAGE_TEXT]}"
         raise TypeError
     finally:
         if sys.exc_info()[0] in (KeyError, TypeError, ValueError):
@@ -84,20 +99,56 @@ def parse_port_in_cmd(arg: list):
             logger.critical(f'Произошла ошибка {sys.exc_info()[0]}, установлен порт {port}')
             return port
 
+@logs
+def parse_mode_in_cmd(arg: list):
+    logger.debug(f'Получен аргумент {arg}')
+    try:
+        if isinstance(arg, list):
+            if arg[3]:
+                mod = arg[3]
+                return mod
+            raise IndexError
+        raise TypeError
+    finally:
+        if sys.exc_info()[0] in (IndexError, TypeError, ValueError):
+            port = int(DEFAULT_PORT)
+            logger.critical(f'Произошла ошибка {sys.exc_info()[0]}, установлен порт {port}')
+            return port
 
 def main():
     addres = parse_addres_in_cmd(sys.argv)
     port = parse_port_in_cmd(sys.argv)
+    mod = parse_mode_in_cmd(sys.argv)
     logger.info(f'Сокет будет привязан к  {(addres, port)}')
-    clientsock = socket(AF_INET, SOCK_STREAM)
-    clientsock.connect((addres, port))
-    presence_message = presence_msg()
-    logger.info('Отправляю сообщение на сервер')
-    send_message(presence_message, clientsock)
-    logger.info('Получаю сообщение с сервера')
-    data = get_message(clientsock)
-    logger.info('Разбираю ответ сервера')
-    print(parsing_msg(data))
+    with socket(AF_INET, SOCK_STREAM) as clientsock:
+        clientsock.connect((addres, port))
+        clientsock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        # presence_message = presence_msg()
+        # logger.info('Отправляю presence-сообщение на сервер')
+        # send_message(presence_message, clientsock)
+        # logger.info('Получаю ответ сервера')
+        # data = get_message(clientsock)
+        # print(parsing_msg(data))
+
+
+        if mod == 'get':
+            logger.debug('Жду данные от сервера')
+            while True:
+                print("+" * 50)
+                data = get_message(clientsock)
+                print(data)
+                logger.info('Разбираю ответ сервера')
+                print(parsing_msg(data))
+
+        if mod == 'send':
+            name = input('Введите свое имя: ')
+            while True:
+                logger.info('Формирую сообщение пользователя')
+                str = input('Введите сообщение для отправки (для выхода введите \'exit\'): ')
+                message = generation_msg(str, name)
+                logger.info('Отправляю сообщение на сервер')
+                send_message(message, clientsock)
+
 
 
 if __name__ == '__main__':
---
+++ lancher.py
@@ -0,0 +1,27 @@
+import time
+from subprocess import Popen, CREATE_NEW_CONSOLE
+
+p_list = []  # Список клиентских процессов
+
+while True:
+    user = input("Запустить 4 клиентов (s) / Закрыть клиентов (x) / Выйти (q) ")
+
+    if user == 'q':
+        break
+    elif user == 's':
+            p_list.append(Popen('python server.py',
+                                creationflags=CREATE_NEW_CONSOLE))
+            time.sleep(1)
+            for _ in range(2):
+                p_list.append(Popen('python client.py 127.0.0.1 7777 send',
+                                creationflags=CREATE_NEW_CONSOLE))
+
+            for _ in range(2):
+                p_list.append(Popen('python client.py 127.0.0.1 7777 get',
+                                creationflags=CREATE_NEW_CONSOLE))
+
+            print(' Запущено 4 клиента')
+    elif user == 'x':
+        for p in p_list:
+            p.kill()
+        p_list.clear()
