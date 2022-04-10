216263930962f58cacdc3b008811173452052051
Fedortsov-AV <aleksandrfedorcov431@gmail.com>
2022-04-10 10:32:11 +1000

    Исправлен ланчер, теперь он запускает только GUI сервера

--- lancher.py
+++ lancher.py
@@ -13,11 +13,11 @@ def main():
         if user == 'q':
             break
         elif user == 's':
-            p_list.append(Popen('python server_script.py.py', creationflags=CREATE_NEW_CONSOLE))
+            p_list.append(Popen('python serverUI.py', creationflags=CREATE_NEW_CONSOLE))
             time.sleep(2)
-            for _ in range(count):
-                p_list.append(Popen('python client.py 127.0.0.1 7777', creationflags=CREATE_NEW_CONSOLE))
-            print(f'Запущено {count} клиента и сервер')
+            # for _ in range(count):
+            #     p_list.append(Popen('python client.py 127.0.0.1 7777', creationflags=CREATE_NEW_CONSOLE))
+            # print(f'Запущено {count} клиента и сервер')
         elif user == 'x':
             for p in p_list:
                 p.kill()
